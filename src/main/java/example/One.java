ae6b066fadff90a7c17175d6dafb11421137c5c4
hongqizl <hongqi@hzzl.net>
2018-06-20 20:18:08 +0800

    2222

--- src/main/java/example/One.java
+++ src/main/java/example/One.java
@@ -4,7 +4,7 @@ public class One {
     String message = "foo";
 
     public String foo() {
-        System.out.println(22223456);
+        System.out.println(2222);
         return message;
     }
 
